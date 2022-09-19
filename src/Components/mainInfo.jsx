dc4c958c2b0162926a2c6c50fd645b2b8ea68fe4
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-09-19 09:45:23 +0530

    social media menu is done

--- src/Components/mainInfo.jsx
+++ src/Components/mainInfo.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+// import "react-simple-typewriter/dist/index.css";
 
 const MainInfo = () => {
   return (
@@ -7,15 +9,43 @@ const MainInfo = () => {
         <div className="left-home">
           <div>
             <p className="profile-txt">
-              Hey 
+              Hey
               <img
                 className="hello-image"
                 src="./images/hello.gif"
                 alt="divu"
               />
               ,<br />
-              I'm <span className="my-name">Divyesh Mavadiya</span> <br />
-              <span className="about-txt">And i am into Web Development!</span>
+              I'm{" "}
+              <span className="my-name">
+                {/* <Typewriter
+                  loop
+                  cursor
+                  cursorStyle=""
+                  typeSpeed={80}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                  words={["Mavadiya Divyesh"]}
+                />{" "} */}
+                Mavadiya Divyesh
+              </span>{" "}
+              <br />
+              <span className="about-txt">
+                And i am a{" "}
+                <Typewriter
+                  loop
+                  cursor
+                  cursorStyle=""
+                  typeSpeed={80}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                  words={[
+                    "Software Developer",
+                    "Web Developer",
+                    "Full-stack Developer",
+                  ]}
+                />{" "}
+              </span>
               <p className="about-me">
                 I have recently started my journey as a Web Developer. Web
                 Development has
@@ -28,6 +58,49 @@ const MainInfo = () => {
                 Talk less do more...shh.
               </p>
             </p>
+          </div>
+          <div className="social-menu">
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/divyesh-mavadiya-a8a995201/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://github.com/codewithdivu"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-github" />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://www.instagram.com/codewithdivu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-instagram" />
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://twitter.com/Divyesh_017"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-twitter" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="right-home">
