a98d39f3abc7eaa4355bdcc1758014fccc9d7a09
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-09-22 01:41:09 +0530

    upto this all done but design in still not done

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -3,7 +3,38 @@ import React from "react";
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <section className="contact">
+        <div className="contact-div">
+          <div className="contact-img-div">
+            <img className="contact-img" src="./images/contact.png" />
+          </div>
+        </div>
+        <div className="form-div">
+          <div className="form">
+            <form id="myform">
+              <div class="user-box">
+                <input type="text" id="name" />
+                <label className="floating-label">Your Name*</label>
+              </div>
+              <div class="user-box">
+                <input type="text" id="email" />
+                <label>Email Address*</label>
+              </div>
+              <div class="user-box">
+                <textarea id="message"></textarea>
+                <label>Enter your message*</label>
+              </div>
+              <a id="submit">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+              </a>
+            </form>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
