7d5beee679804c84f39d9f993fd85da6c525643e
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-09-23 08:53:26 +0530

    upto this project components is done

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -3,15 +3,86 @@ import { Carousel } from "3d-react-carousal";
 
 const Projects = () => {
   let slides = [
-    <img src="https://picsum.photos/800/300/?random" alt="1" />,
-    <img src="https://picsum.photos/800/301/?random" alt="2" />,
-    <img src="https://picsum.photos/800/302/?random" alt="3" />,
-    <img src="https://picsum.photos/800/303/?random" alt="4" />,
-    <img src="https://picsum.photos/800/304/?random" alt="5" />,
+    <div className="project-links">
+      <img src="./images/portfolio.png" alt="1" />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/codewithdivu/my-personal-portfolio"
+      >
+        <span></span>
+        Source Code
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/codewithdivu/my-personal-portfolio"
+      >
+        <span></span>
+        Live View
+      </a>
+    </div>,
+    <div className="project-links">
+      <img src="./images/inote.png" alt="1" />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/codewithdivu/I-Notes-webapp"
+      >
+        <span></span>
+        Source Code
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://candid-praline-15cce8.netlify.app/"
+      >
+        <span></span>
+        Live View
+      </a>
+    </div>,
+    <div className="project-links">
+      <img src="./images/school.png" alt="1" />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/codewithdivu/School-Administration-webApp"
+      >
+        <span></span>
+        Source Code
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://school-administration.vercel.app/dashboard/app"
+      >
+        <span></span>
+        Live View
+      </a>
+    </div>,
+    <div className="project-links">
+      <img src="./images/meco.png" alt="1" />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/codewithdivu/Meco"
+      >
+        <span></span>
+        Source Code
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://meco.vercel.app/movies"
+      >
+        <span></span>
+        Live View
+      </a>
+    </div>,
   ];
   return (
     <div className="project">
-      <Carousel slides={slides} autoplay={true} interval={2000} />
+      <Carousel slides={slides} autoplay={true} interval={10000} />
     </div>
   );
 };
